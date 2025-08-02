7fa379fb84de240283944efdbf655d5bb21a739b
Osaretin Frank <osaretin.frank10@gmail.com>
2025-08-02 16:50:48 +0100

    add tron tokens

--- utils/tokens.ts
+++ utils/tokens.ts
@@ -15,3 +15,21 @@ export const evm_tokens = [
     address: "0x7073BFad0E8d9e15B50F50B5025D6B32A332b35c",
   },
 ]
+
+export const tron_tokens = [
+  {
+    name: "USDC",
+    symbol: "tUSDC",
+    address: "410e957f97bf815bd43e07b477752737ef923a22ab",
+  },
+  {
+    name: "USDT",
+    symbol: "tUSDT",
+    address: "412832a19fefcfd0fe45c3246dd98a839f05845baa",
+  },
+  {
+    name: "DAI",
+    symbol: "tDAI",
+    address: "416aa24bf3b51b90a4f980d832b52a59d6164f4116",
+  },
+]
