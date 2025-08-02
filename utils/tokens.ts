b74059640850e0cb52a6dfc52860ea569a737fd7
Osaretin Frank <osaretin.frank10@gmail.com>
2025-08-02 16:00:46 +0100

    add evm tokens

---
+++ utils/tokens.ts
@@ -0,0 +1,17 @@
+export const evm_tokens = [
+  {
+    name: "USDC",
+    symbol: "tUSDC",
+    address: "0x3D43314A5adE203256172e8b416c6017CA419003",
+  },
+  {
+    name: "USDT",
+    symbol: "tUSDT",
+    address: "0xab0c0C22b3B4C3De6275E8f20Ab02763509e4Ace",
+  },
+  {
+    name: "DAI",
+    symbol: "tDAI",
+    address: "0x7073BFad0E8d9e15B50F50B5025D6B32A332b35c",
+  },
+]
