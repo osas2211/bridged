169b54318b4f79f91ddbb455e146218ff67f2418
Osaretin Frank <osaretin.frank10@gmail.com>
2025-08-03 10:30:21 +0100

    add withdraw ETH script

---
+++ scripts/evm/withdraw_eth.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat")
+const fs = require("fs")
+require("dotenv").config()
+;(async () => {
+  const { swapId, preimage } = JSON.parse(fs.readFileSync("swapData.json"))
+  const provider = new ethers.providers.JsonRpcProvider(process.env.SEP_RPC)
+  const signer = new ethers.Wallet(process.env.EVM_PRIVATE_KEY, provider)
+  const htlc = new ethers.Contract(
+    process.env.ETH_HTLC_ADDRESS,
+    ["function withdraw(bytes32,bytes32)"],
+    signer
+  )
+  const tx = await htlc.withdraw(swapId, preimage)
+  console.log("Withdraw ETH side tx:", tx.hash)
+})()
